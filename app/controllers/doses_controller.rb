1adfad312592c2bb5775673d40c33b209d9cfff4
Guillaume Peterschmitt <guillaume.peterschmitt@gmail.com>
2019-05-22 12:03:29 +0200

    Rake cleaning before day 2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,15 @@
 class DosesController < ApplicationController
- before_action :set_cocktail
+# before_action :set_cocktail
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = @cocktail
-    @dose.ingredient = Ingredient.find(params[:dose][:ingredient])
+    # @dose.cocktail = @cocktail
+    # @dose.ingredient = Ingredient.find(params[:dose][:ingredient_id])
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -18,7 +20,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
@@ -28,8 +30,6 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
-
-
